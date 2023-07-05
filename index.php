0b63a4c72f5d98a73673cb0fe652d26cfca89ea5
César Fernández <kaesar84@icloud.com>
2023-07-05 16:04:58 +0200

    estructura

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="styles/css/index.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+</head>
+
+<body>
+
+    <?php
+    echo '<header><h1>PHP - Udemy</h1></header>'
+    ?>
+
+    <section id="indice">
+        <h2>Índice</h2>
+        <ul>
+            <li>
+                <i class="bi bi-play-circle-fill"></i>
+                <i class="bi bi-6-circle-fill inumero"></i>
+                <a href="teoria/variables.php">Variables</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <i class="bi bi-play-circle-fill"></i>
+                <i class="bi bi-7-circle-fill inumero"></i>
+                <a href="teoria/constantes.php">Constantes</a>
+            </li>
+        </ul>
+
+        <ul>
+            <li>
+                <i class="bi bi-play-circle-fill"></i>
+                <i class="bi bi-8-circle-fill inumero"></i>
+                <a href="teoria/arrays.php">Arrays</a>
+            </li>
+        </ul>
+    </section>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
